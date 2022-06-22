b9226f4e3d7ddca1de0a8972fdb66e08d3b50483
Marcel Jansen <marcel8060@gmail.com>
2022-06-22 08:59:14 +0200

    test: fix failing test by implementing validate logic

--- StringLibrary/StringLibrary.cs
+++ StringLibrary/StringLibrary.cs
@@ -4,6 +4,22 @@ public class StringLibrary
 {
     public Boolean Validate(string stringToValidate, string characterPairs)
     {
-        return true;
+        string openingCharacters = new(characterPairs.Where((x, i) => i % 2 == 0).ToArray());
+        string closingCharacters = new(characterPairs.Where((x, i) => i % 2 != 0).ToArray());
+
+        Stack<char> openingCharactersInString = new Stack<char>();
+
+        foreach (char c in stringToValidate)
+        {
+            if (openingCharacters.Contains(c))
+                openingCharactersInString.Push(c);
+            else if (closingCharacters.Contains(c))
+                openingCharactersInString.Pop();
+        }
+
+        if(openingCharactersInString.Count == 0)
+            return true;
+        else
+            return false;
     }
 }
