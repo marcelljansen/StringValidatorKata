12e6828d676f5efafb9f14e4ebd66f6f563ffad3
Marcel Jansen <marcel8060@gmail.com>
2022-06-22 11:16:24 +0200

    test: fix failing test by extending validate logic

--- StringLibrary/StringLibrary.cs
+++ StringLibrary/StringLibrary.cs
@@ -4,17 +4,27 @@ public class StringLibrary
 {
     public Boolean Validate(string stringToValidate, string characterPairs)
     {
-        string openingCharacters = new(characterPairs.Where((x, i) => i % 2 == 0).ToArray());
-        string closingCharacters = new(characterPairs.Where((x, i) => i % 2 != 0).ToArray());
-
         Stack<char> openingCharactersInString = new Stack<char>();
+        Dictionary<char, char> matchingCharacterPairs = new Dictionary<char, char>();
+        char matchingClosingCharacter;
+
+        for (int i = 0; i < characterPairs.Length - 1; i+=2)
+        {
+            matchingCharacterPairs.Add(characterPairs[i], characterPairs[i+1]);
+        }
 
         foreach (char c in stringToValidate)
         {
-            if (openingCharacters.Contains(c))
+            if (matchingCharacterPairs.ContainsKey(c))
+            {
                 openingCharactersInString.Push(c);
-            else if (closingCharacters.Contains(c))
-                openingCharactersInString.Pop();
+            } 
+            else if (matchingCharacterPairs.ContainsValue(c))
+            {
+                matchingCharacterPairs.TryGetValue(openingCharactersInString.Peek(), out matchingClosingCharacter);
+                if (matchingClosingCharacter == c)
+                    openingCharactersInString.Pop();
+            }
         }
 
         if(openingCharactersInString.Count == 0)
